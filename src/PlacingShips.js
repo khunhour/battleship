3c0fc00ce3497544f8397c0e5ed4698b526a255e
Khunhour Morm <khonhourmorm@gmail.com>
2021-12-02 20:30:13 +0900

    made random ships placement check surrounding give space when placing ships

--- src/PlacingShips.js
+++ src/PlacingShips.js
@@ -1,13 +1,43 @@
 const PlacingShips = (() => {
+  const isOutOfRange = (coord) => {
+    if (coord[0] > 9 || coord[1] > 9 || coord[0] < 0 || coord[1] < 0) {
+      return true;
+    }
+    return false;
+  };
+
   const checkAlreadyHasShip = (player, coord, length, direction) => {
     let hasShip = false;
     let row = coord[0];
     let col = coord[1];
+
     for (let i = 0; i < length; i++) {
+      // surrrounding coordinates that are valid(no -1 no 10)
+      const surroundingCoord = [
+        [row + 1, col],
+        [row - 1, col],
+        [row, col + 1],
+        [row, col - 1],
+        [row - 1, col - 1],
+        [row - 1, col + 1],
+        [row + 1, col + 1],
+        [row + 1, col - 1],
+      ].filter((element) => !isOutOfRange(element));
+
+      // loop through to check if the surrounding coord has ships
+      for (let j = 0; j < surroundingCoord.length; j++) {
+        const x = surroundingCoord[j][0];
+        const y = surroundingCoord[j][1];
+        if (player.gameboard.board[x][y].hasShip !== '') {
+          hasShip = true;
+        }
+      }
+
+      // check if the actual coord itself has ship
       if (player.gameboard.board[row][col].hasShip !== '') {
         hasShip = true;
-        console.log('hi');
       }
+
       if (direction === 'horizontal') {
         col++;
       } else {
